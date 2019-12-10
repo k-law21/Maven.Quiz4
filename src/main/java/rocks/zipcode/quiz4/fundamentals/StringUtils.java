91c4ace836158ed30ee8c28e51f4b9b1335bf9ff
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-12-09 23:20:13 -0500

    String Utils

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,70 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.*;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length()/2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        return midCase(str, true);
+    }
+
+    private static String midCase(String str, boolean upper){
+        String[] arr = str.split("");
+        arr[str.length()/2] = upper? arr[str.length()/2].toUpperCase() : arr[str.length()/2].toLowerCase();
+        StringBuilder b = new StringBuilder();
+        for(String x : arr){b.append(x);}
+        return b.toString();
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        return midCase(str, false);
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        Set<String> set = new HashSet<>(Arrays.asList(str.split("")));
+        return set.size() == str.length();
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        char[] arr = str.toCharArray();
+        char last = arr[0];
+        for(int i = 1; i < str.length(); i++){
+            if(arr[i] == last) return true;
+            last = arr[i];
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        List<String> letters = new ArrayList<>(Arrays.asList(str.split("")));
+        for(int i = 0; i < letters.size() - 1;){
+            if(letters.get(i).equals(letters.get(i+1))){
+                letters.remove(i);
+                letters.remove(i);
+            } else {
+                i++;
+            }
+        }
+        return String.join("", letters);
     }
 
     public static String invertCasing(String str) {
-        return null;
+        char[] arr = str.toCharArray();
+        int count = 0;
+        for(char x : arr){
+            if(Character.isUpperCase(x)){
+                arr[count] = Character.toLowerCase(x);
+            } else {
+                arr[count] = Character.toUpperCase(x);
+            }
+            count++;
+        }
+        return new String(arr);
     }
 }
